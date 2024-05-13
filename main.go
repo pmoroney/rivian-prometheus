227e48efc05808cdad4c95f5e5a94e7faa5bcc37
Pat Moroney <pat@pat.email>
2024-05-12 23:05:33 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/fatih/camelcase"
+	"github.com/pmoroney/rivian-prometheus/rivian"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type Metrics struct {
+	collectors map[string]*prometheus.GaugeVec
+}
+
+func NewPrometheus() *Metrics {
+	return &Metrics{
+		collectors: make(map[string]*prometheus.GaugeVec),
+	}
+}
+
+func toSnakeCase(s string) string {
+	parts := camelcase.Split(s)
+	for i := 0; i < len(parts); i++ {
+		parts[i] = strings.ToLower(parts[i])
+	}
+	return strings.Join(parts, "_")
+}
+
+func (p *Metrics) CreateMetrics() {
+	var s rivian.VehicleState
+	state := reflect.ValueOf(s)
+	for i := 0; i < state.NumField(); i++ {
+		field := state.Type().Field(i)
+		name := toSnakeCase(field.Name)
+		switch field.Type.Name() {
+		case "FloatValue":
+			p.collectors[name] = prometheus.NewGaugeVec(
+				prometheus.GaugeOpts{
+					Namespace: "rivian",
+					Subsystem: "vehicle_info",
+					Name:      name,
+				},
+				[]string{
+					"vehicle_name",
+				},
+			)
+			prometheus.MustRegister(p.collectors[name])
+		case "StringValue":
+			p.collectors[name] = prometheus.NewGaugeVec(
+				prometheus.GaugeOpts{
+					Namespace: "rivian",
+					Subsystem: "vehicle_info",
+					Name:      name,
+				},
+				[]string{
+					"vehicle_name",
+					"value",
+				},
+			)
+			prometheus.MustRegister(p.collectors[name])
+		case "LocationValue":
+			for _, f := range []string{"latitude", "longitude"} {
+				name := name + "_" + f
+				p.collectors[name] = prometheus.NewGaugeVec(
+					prometheus.GaugeOpts{
+						Namespace: "rivian",
+						Subsystem: "vehicle_info",
+						Name:      name,
+					},
+					[]string{
+						"vehicle_name",
+					},
+				)
+				prometheus.MustRegister(p.collectors[name])
+			}
+		case "LocationErrorValue":
+			for _, f := range []string{"position_vertical", "position_horizontal", "speed", "bearing"} {
+				name := name + "_" + f
+				p.collectors[name] = prometheus.NewGaugeVec(
+					prometheus.GaugeOpts{
+						Namespace: "rivian",
+						Subsystem: "vehicle_info",
+						Name:      name,
+					},
+					[]string{
+						"vehicle_name",
+					},
+				)
+				prometheus.MustRegister(p.collectors[name])
+			}
+		default:
+			fmt.Printf("unknown type: %s: %s\n", field.Type.Name(), name)
+		}
+	}
+
+}
+func (p *Metrics) CollectMetrics(vehicle_name string, v *rivian.VehicleState) {
+	state := reflect.ValueOf(v).Elem()
+	for i := 0; i < state.NumField(); i++ {
+		field := state.Type().Field(i)
+		name := toSnakeCase(field.Name)
+		switch field.Type.Name() {
+		case "FloatValue":
+			p.collectors[name].With(prometheus.Labels{"vehicle_name": vehicle_name}).Set(state.FieldByName(field.Name).FieldByName("Value").Float())
+		case "StringValue":
+			p.collectors[name].Reset()
+			p.collectors[name].With(prometheus.Labels{"vehicle_name": vehicle_name, "value": state.FieldByName(field.Name).FieldByName("Value").String()}).Set(1)
+		case "LocationValue":
+			for _, fname := range []string{"Latitude", "Longitude"} {
+				f := toSnakeCase(fname)
+				p.collectors[name+"_"+f].With(prometheus.Labels{"vehicle_name": vehicle_name}).Set(state.FieldByName(field.Name).FieldByName(fname).Float())
+			}
+		case "LocationErrorValue":
+			for _, fname := range []string{"PositionVertical", "PositionHorizontal", "Speed", "Bearing"} {
+				f := toSnakeCase(fname)
+				p.collectors[name+"_"+f].With(prometheus.Labels{"vehicle_name": vehicle_name}).Set(state.FieldByName(field.Name).FieldByName(fname).Float())
+			}
+		default:
+			fmt.Printf("unknown type: %s: %s\n", field.Type.Name(), name)
+		}
+	}
+
+}
+
+func main() {
+	p := NewPrometheus()
+	p.CreateMetrics()
+	var login bool
+	var sessionFile string
+	flag.BoolVar(&login, "l", false, "login and save session")
+	flag.StringVar(&sessionFile, "s", ".rivian_session", "filename for session storage")
+	ctx := context.Background()
+	c := rivian.NewClient()
+	c.Debug(true)
+	c.ReadSessionData(sessionFile)
+	defer c.WriteSessionData(sessionFile)
+
+	err := c.GetCSRFToken(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if login || c.NeedsLogin() {
+		stdin := bufio.NewReader(os.Stdin)
+		fmt.Println("Please enter your email:")
+		email, _ := stdin.ReadString('\n')
+		email = strings.Replace(email, "\n", "", -1)
+		fmt.Println("Please enter your password:")
+		password, _ := stdin.ReadString('\n')
+		password = strings.Replace(password, "\n", "", -1)
+		otp, err := c.Login(ctx, email, password)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if otp {
+			fmt.Println("Please enter your token:")
+			text, _ := stdin.ReadString('\n')
+			text = strings.Replace(text, "\n", "", -1)
+			err = c.ValidateOTP(ctx, email, text)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	vehicles, err := c.GetVehicles(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan struct{})
+	ticker := time.NewTicker(30 * time.Second)
+
+	go func(vehicles []rivian.Vehicle) {
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				for _, v := range vehicles {
+					state, err := c.GetVehicleState(ctx, v)
+					if err != nil {
+						log.Println(err)
+						return
+					}
+					p.CollectMetrics(v.Name, state)
+				}
+			}
+		}
+	}(vehicles)
+
+	http.Handle("/metrics", promhttp.Handler())
+	err = http.ListenAndServe(":9666", nil)
+	done <- struct{}{}
+	ticker.Stop()
+	log.Fatal(err)
+}
---
+++ rivian/rivian.go
@@ -0,0 +1,380 @@
+package rivian
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/hasura/go-graphql-client"
+)
+
+type SessionTokens struct {
+	CSRF    string
+	App     string
+	User    string
+	Access  string
+	Refresh string
+	OTP     string
+}
+
+type Client struct {
+	client     *graphql.Client
+	tokens     SessionTokens
+	httpClient *http.Client
+}
+
+func NewClient() *Client {
+	c := &Client{}
+	c.httpClient = &http.Client{
+		Transport: &loggingTransport{},
+	}
+	c.client = graphql.NewClient("https://rivian.com/api/gql/gateway/graphql", c.httpClient).WithRequestModifier(c.AddHeaders)
+	return c
+}
+
+func (c *Client) NeedsLogin() bool {
+	return c.tokens.User == ""
+}
+
+func (c *Client) Debug(on bool) {
+	c.client = c.client.WithDebug(on)
+	c.httpClient.Transport.(*loggingTransport).Log = true
+}
+
+func (c *Client) AddHeaders(req *http.Request) {
+	if c.tokens.App != "" {
+		req.Header.Set("a-sess", c.tokens.App)
+	}
+	if c.tokens.User != "" {
+		req.Header.Set("u-sess", c.tokens.User)
+	}
+	if c.tokens.CSRF != "" {
+		req.Header.Set("csrf-token", c.tokens.CSRF)
+	}
+	req.Header.Set("Apollographql-Client-Name", "com.rivian.ios.consumer-apollo-ios")
+	req.Header.Set("User-Agent", "RivianApp/707 CFNetwork/1237 Darwin/20.4.0")
+	req.Header.Set("Dc-Cid", "m-ios-"+uuid.New().String())
+}
+
+func (c *Client) GetCSRFToken(ctx context.Context) error {
+	type CreateCSRFToken struct {
+		CreateCsrfToken struct {
+			CsrfToken       string
+			AppSessionToken string
+		} `json:"createCsrfToken"`
+	}
+
+	var resp CreateCSRFToken
+	err := c.client.Mutate(ctx, &resp, map[string]interface{}{}, graphql.OperationName("CreateCSRFToken"))
+	if err != nil {
+		log.Printf("error: %#v", err)
+		return err
+	}
+
+	c.tokens.CSRF = resp.CreateCsrfToken.CsrfToken
+	c.tokens.App = resp.CreateCsrfToken.AppSessionToken
+	return nil
+}
+
+func (c *Client) Login(ctx context.Context, email string, password string) (bool, error) {
+	type Login struct {
+		Login struct {
+			MobileLoginResponse struct {
+				AccessToken      string `json:"accessToken"`
+				RefreshToken     string `json:"refreshToken"`
+				UserSessionToken string `json:"userSessionToken"`
+			} `graphql:"... on MobileLoginResponse"`
+			MobileMFALoginResponse struct {
+				OTPToken string `json:"otpToken"`
+			} `graphql:"... on MobileMFALoginResponse"`
+		} `graphql:"login(email: $email, password: $password)"`
+	}
+
+	var resp Login
+	variables := map[string]interface{}{
+		"email":    email,
+		"password": password,
+	}
+	err := c.client.Mutate(ctx, &resp, variables, graphql.OperationName("Login"))
+	if err != nil {
+		log.Printf("error: %#v", err)
+		return false, err
+	}
+
+	// if no MFA enabled
+	if resp.Login.MobileLoginResponse.UserSessionToken != "" {
+		c.tokens.User = resp.Login.MobileLoginResponse.UserSessionToken
+		c.tokens.Access = resp.Login.MobileLoginResponse.AccessToken
+		c.tokens.Refresh = resp.Login.MobileLoginResponse.RefreshToken
+		return false, nil
+	}
+
+	c.tokens.OTP = resp.Login.MobileMFALoginResponse.OTPToken
+	return true, nil
+}
+
+func (c *Client) ValidateOTP(ctx context.Context, email string, otp string) error {
+	type LoginWithOTP struct {
+		LoginWithOTP struct {
+			AccessToken      string `json:"accessToken"`
+			RefreshToken     string `json:"refreshToken"`
+			UserSessionToken string `json:"userSessionToken"`
+		} `graphql:"loginWithOTP(email: $email, otpCode: $otpCode, otpToken: $otpToken)"`
+	}
+
+	var resp LoginWithOTP
+	variables := map[string]interface{}{
+		"email":    email,
+		"otpCode":  otp,
+		"otpToken": c.tokens.OTP,
+	}
+	err := c.client.Mutate(ctx, &resp, variables, graphql.OperationName("LoginWithOTP"))
+	if err != nil {
+		log.Printf("error: %#v", err)
+		return err
+	}
+
+	c.tokens.User = resp.LoginWithOTP.UserSessionToken
+	c.tokens.Access = resp.LoginWithOTP.AccessToken
+	c.tokens.Refresh = resp.LoginWithOTP.RefreshToken
+	return nil
+
+}
+
+type Vehicle struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (c *Client) GetVehicles(ctx context.Context) ([]Vehicle, error) {
+	type GetUserInfo struct {
+		CurrentUser struct {
+			ID       string    `json:"id"`
+			Vehicles []Vehicle `json:"vehicles"`
+		} `graphql:"currentUser"`
+	}
+
+	var resp GetUserInfo
+	variables := map[string]interface{}{}
+	err := c.client.Query(ctx, &resp, variables, graphql.OperationName("getUserInfo"))
+	if err != nil {
+		log.Printf("error: %#v", err)
+		return nil, err
+	}
+
+	return resp.CurrentUser.Vehicles, nil
+
+}
+
+type FloatValue struct {
+	Value     float64   `json:"value,omitempty"`
+	Timestamp time.Time `json:"timeStamp,omitempty" graphql:"timeStamp"`
+}
+
+type StringValue struct {
+	Value     string    `json:"value,omitempty"`
+	Timestamp time.Time `json:"timeStamp,omitempty" graphql:"timeStamp"`
+}
+
+type LocationValue struct {
+	Latitude  float64   `json:"latitude,omitempty"`
+	Longitude float64   `json:"longitude,omitempty"`
+	Timestamp time.Time `json:"timeStamp,omitempty" graphql:"timeStamp"`
+}
+
+type LocationErrorValue struct {
+	PositionVertical   float64   `json:"positionVertical,omitempty"`
+	PositionHorizontal float64   `json:"positionHorizontal,omitempty"`
+	Speed              float64   `json:"speed,omitempty"`
+	Bearing            float64   `json:"bearing,omitempty"`
+	Timestamp          time.Time `json:"timeStamp,omitempty" graphql:"timeStamp"`
+}
+
+type VehicleState struct {
+	GNSSLocation                      LocationValue      `json:"gnssLocation,omitempty"`
+	GNSSError                         LocationErrorValue `json:"gnssError,omitempty"`
+	AlarmSoundStatus                  StringValue        `json:"alarmSoundStatus,omitempty"`
+	BatteryCapacity                   FloatValue         `json:"batteryCapacity,omitempty"`
+	BatteryHvThermalEvent             StringValue        `json:"batteryHvThermalEvent,omitempty"`
+	BatteryHvThermalEventPropagation  StringValue        `json:"batteryHvThermalEventPropagation,omitempty"`
+	BatteryLevel                      FloatValue         `json:"batteryLevel,omitempty"`
+	BatteryLimit                      FloatValue         `json:"batteryLimit,omitempty"`
+	BrakeFluidLow                     StringValue        `json:"brakeFluidLow,omitempty"`
+	BtmFfHardwareFailureStatus        StringValue        `json:"btmFfHardwareFailureStatus,omitempty"`
+	BtmIcHardwareFailureStatus        StringValue        `json:"btmIcHardwareFailureStatus,omitempty"`
+	BtmLfdHardwareFailureStatus       StringValue        `json:"btmLfdHardwareFailureStatus,omitempty"`
+	BtmRfHardwareFailureStatus        StringValue        `json:"btmRfHardwareFailureStatus,omitempty"`
+	BtmRfdHardwareFailureStatus       StringValue        `json:"btmRfdHardwareFailureStatus,omitempty"`
+	CabinClimateDriverTemperature     FloatValue         `json:"cabinClimateDriverTemperature,omitempty"`
+	CabinClimateInteriorTemperature   FloatValue         `json:"cabinClimateInteriorTemperature,omitempty"`
+	CabinPreconditioningStatus        StringValue        `json:"cabinPreconditioningStatus,omitempty"`
+	CabinPreconditioningType          StringValue        `json:"cabinPreconditioningType,omitempty"`
+	CarWashMode                       StringValue        `json:"carWashMode,omitempty"`
+	ChargePortState                   StringValue        `json:"chargePortState,omitempty"`
+	ChargerDerateStatus               StringValue        `json:"chargerDerateStatus,omitempty"`
+	ChargerState                      StringValue        `json:"chargerState,omitempty"`
+	ChargerStatus                     StringValue        `json:"chargerStatus,omitempty"`
+	ClosureFrunkClosed                StringValue        `json:"closureFrunkClosed,omitempty"`
+	ClosureFrunkLocked                StringValue        `json:"closureFrunkLocked,omitempty"`
+	ClosureFrunkNextAction            StringValue        `json:"closureFrunkNextAction,omitempty"`
+	ClosureLiftgateClosed             StringValue        `json:"closureLiftgateClosed,omitempty"`
+	ClosureLiftgateLocked             StringValue        `json:"closureLiftgateLocked,omitempty"`
+	ClosureLiftgateNextAction         StringValue        `json:"closureLiftgateNextAction,omitempty"`
+	ClosureSideBinLeftClosed          StringValue        `json:"closureSideBinLeftClosed,omitempty"`
+	ClosureSideBinLeftLocked          StringValue        `json:"closureSideBinLeftLocked,omitempty"`
+	ClosureSideBinLeftNextAction      StringValue        `json:"closureSideBinLeftNextAction,omitempty"`
+	ClosureSideBinRightClosed         StringValue        `json:"closureSideBinRightClosed,omitempty"`
+	ClosureSideBinRightLocked         StringValue        `json:"closureSideBinRightLocked,omitempty"`
+	ClosureSideBinRightNextAction     StringValue        `json:"closureSideBinRightNextAction,omitempty"`
+	ClosureTailgateClosed             StringValue        `json:"closureTailgateClosed,omitempty"`
+	ClosureTailgateLocked             StringValue        `json:"closureTailgateLocked,omitempty"`
+	ClosureTailgateNextAction         StringValue        `json:"closureTailgateNextAction,omitempty"`
+	ClosureTonneauClosed              StringValue        `json:"closureTonneauClosed,omitempty"`
+	ClosureTonneauLocked              StringValue        `json:"closureTonneauLocked,omitempty"`
+	ClosureTonneauNextAction          StringValue        `json:"closureTonneauNextAction,omitempty"`
+	DefrostDefogStatus                StringValue        `json:"defrostDefogStatus,omitempty"`
+	DistanceToEmpty                   FloatValue         `json:"distanceToEmpty,omitempty"`
+	DoorFrontLeftClosed               StringValue        `json:"doorFrontLeftClosed,omitempty"`
+	DoorFrontLeftLocked               StringValue        `json:"doorFrontLeftLocked,omitempty"`
+	DoorFrontRightClosed              StringValue        `json:"doorFrontRightClosed,omitempty"`
+	DoorFrontRightLocked              StringValue        `json:"doorFrontRightLocked,omitempty"`
+	DoorRearLeftClosed                StringValue        `json:"doorRearLeftClosed,omitempty"`
+	DoorRearLeftLocked                StringValue        `json:"doorRearLeftLocked,omitempty"`
+	DoorRearRightClosed               StringValue        `json:"doorRearRightClosed,omitempty"`
+	DoorRearRightLocked               StringValue        `json:"doorRearRightLocked,omitempty"`
+	DriveMode                         StringValue        `json:"driveMode,omitempty"`
+	GearGuardLocked                   StringValue        `json:"gearGuardLocked,omitempty"`
+	GearGuardVideoMode                StringValue        `json:"gearGuardVideoMode,omitempty"`
+	GearGuardVideoStatus              StringValue        `json:"gearGuardVideoStatus,omitempty"`
+	GearGuardVideoTermsAccepted       StringValue        `json:"gearGuardVideoTermsAccepted,omitempty"`
+	GearStatus                        StringValue        `json:"gearStatus,omitempty"`
+	GnssAltitude                      FloatValue         `json:"gnssAltitude,omitempty"`
+	GnssBearing                       FloatValue         `json:"gnssBearing,omitempty"`
+	GnssSpeed                         FloatValue         `json:"gnssSpeed,omitempty"`
+	LimitedAccelCold                  FloatValue         `json:"limitedAccelCold,omitempty"`
+	LimitedRegenCold                  FloatValue         `json:"limitedRegenCold,omitempty"`
+	OtaAvailableVersion               StringValue        `json:"otaAvailableVersion,omitempty"`
+	OtaAvailableVersionGitHash        StringValue        `json:"otaAvailableVersionGitHash,omitempty"`
+	OtaAvailableVersionNumber         FloatValue         `json:"otaAvailableVersionNumber,omitempty"`
+	OtaAvailableVersionWeek           FloatValue         `json:"otaAvailableVersionWeek,omitempty"`
+	OtaAvailableVersionYear           FloatValue         `json:"otaAvailableVersionYear,omitempty"`
+	OtaCurrentStatus                  StringValue        `json:"otaCurrentStatus,omitempty"`
+	OtaCurrentVersion                 StringValue        `json:"otaCurrentVersion,omitempty"`
+	OtaCurrentVersionGitHash          StringValue        `json:"otaCurrentVersionGitHash,omitempty"`
+	OtaCurrentVersionNumber           FloatValue         `json:"otaCurrentVersionNumber,omitempty"`
+	OtaCurrentVersionWeek             FloatValue         `json:"otaCurrentVersionWeek,omitempty"`
+	OtaCurrentVersionYear             FloatValue         `json:"otaCurrentVersionYear,omitempty"`
+	OtaDownloadProgress               FloatValue         `json:"otaDownloadProgress,omitempty"`
+	OtaInstallDuration                FloatValue         `json:"otaInstallDuration,omitempty"`
+	OtaInstallProgress                FloatValue         `json:"otaInstallProgress,omitempty"`
+	OtaInstallReady                   StringValue        `json:"otaInstallReady,omitempty"`
+	OtaInstallTime                    FloatValue         `json:"otaInstallTime,omitempty"`
+	OtaInstallType                    StringValue        `json:"otaInstallType,omitempty"`
+	OtaStatus                         StringValue        `json:"otaStatus,omitempty"`
+	PetModeStatus                     StringValue        `json:"petModeStatus,omitempty"`
+	PetModeTemperatureStatus          StringValue        `json:"petModeTemperatureStatus,omitempty"`
+	PowerState                        StringValue        `json:"powerState,omitempty"`
+	RangeThreshold                    StringValue        `json:"rangeThreshold,omitempty"`
+	RearHitchStatus                   StringValue        `json:"rearHitchStatus,omitempty"`
+	RemoteChargingAvailable           FloatValue         `json:"remoteChargingAvailable,omitempty"`
+	SeatFrontLeftHeat                 StringValue        `json:"seatFrontLeftHeat,omitempty"`
+	SeatFrontLeftVent                 StringValue        `json:"seatFrontLeftVent,omitempty"`
+	SeatFrontRightHeat                StringValue        `json:"seatFrontRightHeat,omitempty"`
+	SeatFrontRightVent                StringValue        `json:"seatFrontRightVent,omitempty"`
+	SeatRearLeftHeat                  StringValue        `json:"seatRearLeftHeat,omitempty"`
+	SeatRearRightHeat                 StringValue        `json:"seatRearRightHeat,omitempty"`
+	SeatThirdRowLeftHeat              StringValue        `json:"seatThirdRowLeftHeat,omitempty"`
+	SeatThirdRowRightHeat             StringValue        `json:"seatThirdRowRightHeat,omitempty"`
+	ServiceMode                       StringValue        `json:"serviceMode,omitempty"`
+	SteeringWheelHeat                 StringValue        `json:"steeringWheelHeat,omitempty"`
+	TimeToEndOfCharge                 FloatValue         `json:"timeToEndOfCharge,omitempty"`
+	TirePressureStatusFrontLeft       StringValue        `json:"tirePressureStatusFrontLeft,omitempty"`
+	TirePressureStatusFrontRight      StringValue        `json:"tirePressureStatusFrontRight,omitempty"`
+	TirePressureStatusRearLeft        StringValue        `json:"tirePressureStatusRearLeft,omitempty"`
+	TirePressureStatusRearRight       StringValue        `json:"tirePressureStatusRearRight,omitempty"`
+	TirePressureStatusValidFrontLeft  StringValue        `json:"tirePressureStatusValidFrontLeft,omitempty"`
+	TirePressureStatusValidFrontRight StringValue        `json:"tirePressureStatusValidFrontRight,omitempty"`
+	TirePressureStatusValidRearLeft   StringValue        `json:"tirePressureStatusValidRearLeft,omitempty"`
+	TirePressureStatusValidRearRight  StringValue        `json:"tirePressureStatusValidRearRight,omitempty"`
+	TrailerStatus                     StringValue        `json:"trailerStatus,omitempty"`
+	TwelveVoltBatteryHealth           StringValue        `json:"twelveVoltBatteryHealth,omitempty"`
+	VehicleMileage                    FloatValue         `json:"vehicleMileage,omitempty"`
+	WindowFrontLeftCalibrated         StringValue        `json:"windowFrontLeftCalibrated,omitempty"`
+	WindowFrontLeftClosed             StringValue        `json:"windowFrontLeftClosed,omitempty"`
+	WindowFrontRightCalibrated        StringValue        `json:"windowFrontRightCalibrated,omitempty"`
+	WindowFrontRightClosed            StringValue        `json:"windowFrontRightClosed,omitempty"`
+	WindowRearLeftCalibrated          StringValue        `json:"windowRearLeftCalibrated,omitempty"`
+	WindowRearLeftClosed              StringValue        `json:"windowRearLeftClosed,omitempty"`
+	WindowRearRightCalibrated         StringValue        `json:"windowRearRightCalibrated,omitempty"`
+	WindowRearRightClosed             StringValue        `json:"windowRearRightClosed,omitempty"`
+	WindowsNextAction                 StringValue        `json:"windowsNextAction,omitempty"`
+	WiperFluidState                   StringValue        `json:"wiperFluidState,omitempty"`
+}
+
+func (c *Client) GetVehicleState(ctx context.Context, v Vehicle) (*VehicleState, error) {
+	type GetVehicleState struct {
+		VehicleState VehicleState `json:"vehicleState" graphql:"vehicleState(id: $vehicleID)"`
+	}
+
+	var resp GetVehicleState
+	variables := map[string]interface{}{
+		"vehicleID": v.ID,
+	}
+	err := c.client.Query(ctx, &resp, variables, graphql.OperationName("GetVehicleState"))
+	if err != nil {
+		log.Printf("error: %#v", err)
+		return nil, err
+	}
+
+	return &resp.VehicleState, nil
+
+}
+
+func (c *Client) WriteSessionData(filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	return encoder.Encode(c.tokens)
+}
+
+func (c *Client) ReadSessionData(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	return decoder.Decode(&c.tokens)
+}
+
+type loggingTransport struct {
+	Log bool
+}
+
+func (s *loggingTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	var bytes []byte
+	if s.Log {
+		bytes, _ = httputil.DumpRequestOut(r, true)
+	}
+
+	resp, err := http.DefaultTransport.RoundTrip(r)
+	// err is returned after dumping the response
+
+	if s.Log {
+		respBytes, _ := httputil.DumpResponse(resp, true)
+		bytes = append(bytes, respBytes...)
+
+		fmt.Printf("%s\n", bytes)
+	}
+
+	return resp, err
+}
